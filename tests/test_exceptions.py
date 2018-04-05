0eb445f392cf37a4114d77f51f8171b16a306e0f
Adrian Vandier Ast <adrian.vandierast@sewan.fr>
2018-04-05 16:39:38 +0200

    tests for exceptions.

---
+++ tests/test_exceptions.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from pysyphe.exceptions import PysypheException, ActionException, TransactionException, WeAreDoomedException
+
+
+def test_PysypheException():
+    assert PysypheException()
+
+
+def test_ActionException():
+    assert ActionException()
+
+
+def test_TransactionException():
+    assert TransactionException()
+
+
+def test_WeAreDoomedException():
+    exc = WeAreDoomedException("fail", ["traceback1", "traceback2"])
+    assert str(exc)
