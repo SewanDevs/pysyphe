24df3330afe437fdd9ccddcd359b519992913414
Adrian Vandier Ast <adrian.vandierast@sewan.fr>
2018-04-06 15:17:25 +0200

    pipeline_test_suite

---
+++ pysyphe/pipeline_test_suite.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+from six import with_metaclass
+
+import types
+import pytest
+from mock import MagicMock
+
+from .actions import Action
+from .transactions import TransactionsManager, PipelineTransactionHandler
+from .exceptions import PysypheException
+
+"""
+    Pipeline Testing How-To:
+
+    To test a pipeline, inherit from PipelineTestSuite in your test file and implements pipeline_creator and success_checker.
+    The name of the class you have created should begin with 'Test' for pytest to collect it.
+    pipeline_creator and success_checker have to be staticmethod.
+    pipeline_creator should return a ActionPipeline instance. Every calls to this function should return a fresh pipeline.
+    success_checker should return a boolean.
+    To add a function fixture to all tests, decorate your inherited class with pytest.mark.usefixtures().
+
+    ```python
+    import pytest
+    from pipeline_helper import PipelineTestSuite
+    import pipelines
+
+    @pytest.mark.usefixtures("fixture_name")
+    class TestPipeline(PipelineTestSuite):
+        @staticmethod
+        def pipeline_creator():
+            return pipelines.generate_pipeline_to_test()
+
+        @staticmethod
+        def success_checker():
+            return pipelines.check_rollback_was_successfull()
+    ```
+"""
+
+
+# This exception should be used or catched by anyone, so we define it here.
+class PipelineTestSuiteActionException(PysypheException):
+    pass
+
+
+def copy_func(func):
+    return types.FunctionType(func.__code__, func.__globals__, func.__name__, func.__defaults__, func.__closure__)
+
+
+class PipelineTestSuiteMetaclass(type):
+    def __new__(mcs, name, bases, attrs):
+        # As described in the comment of PipelineTestSuite.pipeline_with_failure_fixture (you should look at it before),
+        # we need to re-parametrize pipeline_with_failure_fixture in the subclasses because the params depends on the return value
+        # of the implementations of pipeline_creator in thoses subclasses.
+        pipeline_to_test = None
+        if "pipeline_creator" in attrs:
+            pipeline_creator = attrs["pipeline_creator"]
+            pipeline_to_test = pipeline_creator.__func__()
+        if pipeline_to_test:
+            # The class currently constructed defines a pipeline to test.
+            fixture_params = range(len(pipeline_to_test.actions))
+            # Now, we need to retrieve pipeline_with_failure_fixture to re-parametrize it.
+            # It is somewhere in the baseclasses, at least one have it or we are not somewhere in the inheritence tree
+            # of PipelineTestSuite.
+            pipeline_with_failure_fixture = [base.pipeline_with_failure_fixture for base in bases
+                                             if hasattr(base, "pipeline_with_failure_fixture")]
+            if not pipeline_with_failure_fixture:
+                raise Exception("No baseclass has pipeline_with_failure_fixture... Something has gone wrong.")
+            pipeline_with_failure_fixture = pipeline_with_failure_fixture[0]
+            # Now that we have it, we will make a copy of it to avoid breaking the parametrization of other subclasses.
+            # To understand why, you should know that pytest saves the parametrization of a fixture as metadata on the original
+            # function and thus if we re-parametrize the same fixture again, all subclasses will overwrite the same params.
+            copied_func = copy_func(pipeline_with_failure_fixture)
+            attrs["pipeline_with_failure_fixture"] = pytest.fixture(scope='function', params=fixture_params)(copied_func)
+
+        # We have another problem, pytest.mark.usefixtures on subclasses is buggy actually:
+        # See: https://github.com/pytest-dev/pytest/issues/2806, https://github.com/pytest-dev/pytest/issues/568 and
+        # https://github.com/pytest-dev/pytest/issues/535.
+        # So, for each test function in the bases, we make a copy on the current class to permit the use of usefixtures.
+        for base in bases:
+            for attr_name, attr in vars(base).items():
+                if attr_name.startswith("test") and callable(attr):
+                    copied_func = copy_func(attr)
+                    if attr_name not in attrs:  # We want to avoid overwriting something.
+                        attrs[attr_name] = copied_func
+
+        return type.__new__(mcs, name, bases, attrs)
+
+
+class PipelineTestSuite(with_metaclass(PipelineTestSuiteMetaclass, object)):
+
+    @staticmethod
+    # pipeline_creator is called at metaclass instanciation time so it needs to be static
+    def pipeline_creator():
+        """ To be reimplemented """
+        return None
+
+    @staticmethod
+    def success_checker():
+        """ To be reimplemented """
+        return True
+
+    @staticmethod
+    def modify_action(pipeline, action, position):
+        pipeline._actions_pipeline._list[position] = action
+
+    @pytest.fixture(scope="function")
+    def pipeline_fixture(self):
+        if not self.success_checker():
+            # TODO: maybe it should fail... Because skipped test are not seen by developers.
+            pytest.skip("Last test has not been correctly cleaned up...")
+        yield self.pipeline_creator()
+
+    @pytest.fixture(scope="function", params=[])
+    # We know that there is currently no pipeline to test so the params of the fixture are empty.
+    # But, in subclasses, the pipeline to test will not be empty and the fixture should re-parametrized.
+    # This is done thanks to the metaclass PipelineTestSuiteMetaclass.
+    def pipeline_with_failure_fixture(self, pipeline_fixture, request):
+        action_pos_that_fails = request.param
+        action_fct = MagicMock(
+            side_effect=PipelineTestSuiteActionException("Your pipeline has been eaten by your dog ! Try again."))
+        failed_action = Action(action_fct, lambda: None)
+        self.modify_action(pipeline_fixture, failed_action, action_pos_that_fails)
+        yield pipeline_fixture
+
+    def test_pipeline__complete(self, pipeline_fixture):
+        trm = TransactionsManager()
+        pipe_handler = PipelineTransactionHandler(pipeline_fixture)
+        trm.add_transaction_handler(pipe_handler)
+        with trm.begin():
+            trm.execute()
+            trm.rollback()
+        assert self.success_checker()
+
+    def test_pipeline__partial_fails(self, pipeline_with_failure_fixture):
+        trm = TransactionsManager()
+        pipe_handler = PipelineTransactionHandler(pipeline_with_failure_fixture)
+        trm.add_transaction_handler(pipe_handler)
+        with pytest.raises(PipelineTestSuiteActionException):  # Exception of failed action mock.
+            with trm.begin():
+                trm.execute()
+        assert self.success_checker()
---
+++ tests/test_pipeline_test_suite.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from mock import MagicMock
+from _pytest.monkeypatch import MonkeyPatch
+
+from pysyphe.actions import statefull_action, ActionsPipeline
+from pysyphe.streamers import InfoStreamer
+from pysyphe.pipeline_test_suite import PipelineTestSuite
+
+# /!\ DISCLAIMER /!\ Sorry, but you can't call this test partially, you have to call all tests from TestPipelineTestSuite.
+
+# Principle of this test: we will make a two-actions-long pipeline and use the infostreamer to know what was executed/rollbacked.
+
+class InfoStreamerMock(InfoStreamer):
+    def __init__(self):
+        self.steps = []
+
+    def send_info(self, **kwargs):
+        if 'begin' in kwargs:
+            step_type = "begin"
+        if 'end' in kwargs:
+            step_type = "end"
+        self.steps.append((kwargs["action_name"], step_type))
+
+
+info_streamer = InfoStreamerMock()
+
+
+def pipeline_gen():
+    @statefull_action([], name="action1")
+    def action1(state):
+        pass
+
+    @action1.rollback_action([], name="rollback1")
+    def rollback1(state):
+        pass
+
+    @statefull_action([], name="action2")
+    def action2(state):
+        pass
+
+    @action2.rollback_action([], name="rollback2")
+    def rollback2(state):
+        pass
+
+    ap = ActionsPipeline(name="pipeline")
+    ap.rollback_name = "pipeline_rollback"
+    ap.append(action1.get_prepared_action())
+    ap.append(action2.get_prepared_action())
+    ap.set_info_streamer(info_streamer)
+    return ap
+
+
+@pytest.fixture(scope="module")
+def module_monkeypatch():
+    monkeypatch = MonkeyPatch()
+    yield monkeypatch
+    monkeypatch.undo()
+
+
+@pytest.fixture(scope="module")
+def validate_test(module_monkeypatch):
+    # With a two-long pipeline, there is three tests: one complete, and one that makes each action fail.
+    success_checker_mock = MagicMock(return_value=True)
+    module_monkeypatch.setattr(PipelineTestSuite, "success_checker", success_checker_mock)
+    yield
+    # Tests has been done, we can validate now.
+    # PipelineTestSuite replace an action of the pipeline by a fake action (that does not call the infostreamer).
+    complete_test_steps = [
+        ("pipeline", "begin"),
+        ("action1", "begin"),
+        ("action1", "end"),
+        ("action2", "begin"),
+        ("action2", "end"),
+        ("pipeline", "end"),
+        ("pipeline_rollback", "begin"),
+        ("rollback2", "begin"),
+        ("rollback2", "end"),
+        ("rollback1", "begin"),
+        ("rollback1", "end"),
+        ("pipeline_rollback", "end"),
+    ]
+    # partial2: second action is replaced
+    partial1_test_steps = [
+        ("pipeline", "begin"),
+        ("action1", "begin"),
+        ("action1", "end"),
+        ("pipeline", "end"),
+        ("pipeline_rollback", "begin"),
+        ("rollback1", "begin"),
+        ("rollback1", "end"),
+        ("pipeline_rollback", "end"),
+    ]
+    # partial2: first action is replaced
+    partial2_test_steps = [
+        ("pipeline", "begin"),
+        ("pipeline", "end"),
+        ("pipeline_rollback", "begin"),
+        ("pipeline_rollback", "end"),
+    ]
+    # We will use string comparison to look for sublist :D
+    complete_test_steps = ",".join(["-".join(step) for step in complete_test_steps])
+    partial1_test_steps = ",".join(["-".join(step) for step in partial1_test_steps])
+    partial2_test_steps = ",".join(["-".join(step) for step in partial2_test_steps])
+    done_steps = ",".join(["-".join(step) for step in info_streamer.steps])
+    assert complete_test_steps in done_steps
+    assert partial1_test_steps in done_steps
+    assert partial2_test_steps in done_steps
+    assert success_checker_mock.call_count == 6  # twice per test.
+
+
+@pytest.mark.usefixtures("validate_test")
+class TestPipelineTestSuite(PipelineTestSuite):
+    @staticmethod
+    def pipeline_creator():
+        return pipeline_gen()
